72666d9e925d2731ae1da252a9e0344b26083f5a
Bo-Yong <poerevan9@gmail.com>
2025-11-27 13:10:55 +0800

    Add .js extension to import

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@
  * Vercel Serverless Function Entry Point
  */
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "../server/src/vercel-app";
+import app from "../server/src/vercel-app.js";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   return (app as any)(req, res);
